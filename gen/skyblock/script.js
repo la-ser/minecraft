293627e8a14762c2e73e8cb0f2e4353f45d14da7
la-ser <77540680+la-ser@users.noreply.github.com>
2023-04-20 21:20:30 +0200

    Add files via upload

--- gen/skyblock/script.js
+++ gen/skyblock/script.js
@@ -1,3 +1,9 @@
+const showSearch = document.getElementById("searchbox");
+
+function toggleSearch() {
+  showSearch.classList.toggle("hidden");
+}
+
 window.addEventListener("beforeunload", function (event) {
   var checkBox = document.getElementById("check_save");
 
@@ -24,7 +30,9 @@ fromLoad.value = score_min;
 // toLoad.max = score_max;
 toLoad.value = score_max;
 
-addRowBtn.addEventListener("click", () => {
+addRowBtn.addEventListener("click", addRow);
+
+function addRow(iText, iType) {
   const row = table.insertRow(-1);
 
   const tableRows = table.rows;
@@ -36,34 +44,98 @@ addRowBtn.addEventListener("click", () => {
   prevEndValue1 = parseInt(to) + 1;
   prevEndValue2 = parseInt(to) + 2;
 
-  row.innerHTML =
-    `
+  if (iText == undefined || iType == undefined) {
+    // console.log("no item selected");
+
+    row.innerHTML =
+      `
       <td><input type="number" min="1" max=` +
-    score_max +
-    ` value=` +
-    prevEndValue1 +
-    `></td>
+      score_max +
+      ` value=` +
+      prevEndValue1 +
+      `></td>
       <td><input type="number" min="1" max=` +
-    score_max +
-    ` value=` +
-    prevEndValue2 +
-    `></td>
+      score_max +
+      ` value=` +
+      prevEndValue2 +
+      `></td>
       <td><input id="text" type="text" placeholder="dirt / chicken"></td>
       <td>
           <select>
-          <option value="block">Block</option>
-          <option value="summon">Summon</option>
+              <option value="block">Block</option>
+              <option value="summon">Summon</option>
               <option value="other">Other</option>
           </select>
       </td>
       <td><button class="remove-row">Remove</button></td>
   `;
 
-  const removeRowBtn = row.querySelector(".remove-row");
-  removeRowBtn.addEventListener("click", () => {
-    table.deleteRow(row.rowIndex);
-  });
-});
+    const removeRowBtn = row.querySelector(".remove-row");
+    removeRowBtn.addEventListener("click", () => {
+      table.deleteRow(row.rowIndex);
+    });
+  } else {
+    if (iType == "summon") {
+      console.log("summon type");
+      row.innerHTML =
+        `
+      <td><input type="number" min="1" max=` +
+        score_max +
+        ` value=` +
+        prevEndValue1 +
+        `></td>
+      <td><input type="number" min="1" max=` +
+        score_max +
+        ` value=` +
+        prevEndValue2 +
+        `></td>
+      <td><input id="text" type="text" placeholder="dirt / chicken" value="${iText}"></td>
+      <td>
+          <select>
+              <option value="block">Block</option>
+              <option selected value="summon">Summon</option>
+              <option value="other">Other</option>
+          </select>
+      </td>
+      <td><button class="remove-row">Remove</button></td>
+  `;
+
+      const removeRowBtn = row.querySelector(".remove-row");
+      removeRowBtn.addEventListener("click", () => {
+        table.deleteRow(row.rowIndex);
+      });
+    } else if (iType == "block") {
+      console.log("block type");
+      row.innerHTML =
+        `
+      <td><input type="number" min="1" max=` +
+        score_max +
+        ` value=` +
+        prevEndValue1 +
+        `></td>
+      <td><input type="number" min="1" max=` +
+        score_max +
+        ` value=` +
+        prevEndValue2 +
+        `></td>
+      <td><input id="text" type="text" placeholder="dirt / chicken" value="${iText}"></td>
+      <td>
+          <select>
+              <option selected value="block">Block</option>
+              <option value="summon">Summon</option>
+              <option value="other">Other</option>
+          </select>
+      </td>
+      <td><button class="remove-row">Remove</button></td>
+  `;
+
+      const removeRowBtn = row.querySelector(".remove-row");
+      removeRowBtn.addEventListener("click", () => {
+        table.deleteRow(row.rowIndex);
+      });
+    } else return console.log("error.type.not.found");
+  }
+}
 
 generateBtn.addEventListener("click", generateOutput);
 
@@ -98,5 +170,81 @@ function generateOutput() {
 window.addEventListener("keypress", function (event) {
   if (event.keyCode === 13) {
     generateOutput();
+  }
+});
+
+// new (slider)
+
+function search_item() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("mc-list");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "";
+    }
+  }
+}
+
+function item_select(item, type) {
+  addRow(item, type);
+}
+
+const searchBox = document.getElementById("searchbox");
+const slider = document.getElementById("searchSlider");
+
+slider.oninput = function () {
+  searchBox.style.width = this.value + "%";
+};
+
+// replace "_" with " "
+const searchList = document.getElementById("searchList");
+const listItems = searchList.getElementsByTagName("a");
+
+for (let i = 0; i < listItems.length; i++) {
+  const itemText = listItems[i].innerHTML;
+  const updatedText = itemText.replace(/_/g, " ");
+  listItems[i].innerHTML = updatedText;
+}
+
+// category selection
+const allCategory = document.getElementById("mc-all-cat");
+const blocksCategory = document.getElementById("mc-block-cat");
+const summonsCategory = document.getElementById("mc-summon-cat");
+
+allCategory.addEventListener("click", () => {
+  const itemsBlock = document.querySelectorAll(".mc-block");
+  const itemsSummon = document.querySelectorAll(".mc-summon");
+
+  for (const box of itemsSummon) {
+    box.classList.remove("hidden");
+  }
+  for (const box of itemsBlock) {
+    box.classList.remove("hidden");
+  }
+});
+blocksCategory.addEventListener("click", () => {
+  const itemsBlock = document.querySelectorAll(".mc-block");
+  const itemsSummon = document.querySelectorAll(".mc-summon");
+
+  for (const box of itemsSummon) {
+    box.classList.add("hidden");
+  }
+  for (const box of itemsBlock) {
+    box.classList.remove("hidden");
+  }
+});
+summonsCategory.addEventListener("click", () => {
+  const itemsBlock = document.querySelectorAll(".mc-block");
+  const itemsSummon = document.querySelectorAll(".mc-summon");
+
+  for (const box of itemsSummon) {
+    box.classList.remove("hidden");
+  }
+  for (const box of itemsBlock) {
+    box.classList.add("hidden");
   }
 });
